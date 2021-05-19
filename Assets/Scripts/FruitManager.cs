539eb60fa04c698ac3d1fc3f79f5972d231f8ee2
adfermia <adrianfern95@gmail.com>
2021-05-19 17:50:39 +0200

    FirstCommit

---
+++ Assets/Scripts/FruitManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitManager : MonoBehaviour
+{
+    private void Update() {
+        AllFruitsCollected();
+    }
+    public void AllFruitsCollected () {
+        if(transform.childCount == 0){
+           
+        }
+    }
+}
